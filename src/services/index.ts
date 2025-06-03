8ac19daaa0c942869ed99b89831ed3bbfdb5987c
GuiSebastiao <guilhermesebastiaou.u@gmail.com>
2025-06-03 19:32:27 -0300

    adicionada a nova url do backend

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,7 +2,7 @@ import axiosService, { type AxiosInstance } from "axios";
 import type { AuthDTO } from "@/services/types/AuthDTO";
 
 export const axios: AxiosInstance = axiosService.create({
-  baseURL: "https://minerva-flashcards-api.koyeb.app",
+  baseURL: "https://minerva-api-tg8d.onrender.com",
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
